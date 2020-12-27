6548877e9f719b8f2e00547ed87e5ee4bcd122d5
Francis Domingo <francis.domingo@pm.me>
2020-12-27 15:06:33 +0800

    re-arrange blocklets

--- blocks.h
+++ blocks.h
@@ -1,10 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",      "dwmcpu",			       		5,		0},
 	{"",      "dwmspotify",					1,		0},
-	{"",      "dwmdate",			       60,		0},
-	{"",      "dwmvol",			       		1,		0},
+	{"",      "sb-cpu",	            		5,		18},
+	{"",      "sb-battery",            		5,	 	3},
+	{"",      "sb-clock",			       60,		1},
+	{"",      "sb-volume",		       		1,		0},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
