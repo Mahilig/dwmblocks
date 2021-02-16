10d693f451aff84896857418ee93a3115c6fe923
Francis Domingo <francis.domingo@pm.me>
2021-02-16 16:27:11 +0800

    replace spotifyctl with playerctl

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",      "dwmspotify",					1,		0},
+	{"",      "playerctl-spotify",			3,		0},
 	{"",      "sb-cpu",	            		5,		0},
 //	{"",      "sb-battery",            		5,	 	0},     // no battery on desktop
 	{"",      "sb-clock",			       60,		0},
